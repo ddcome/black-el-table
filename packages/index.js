cb0ec72e2a910863f750a88b6f2d2d9171a0b5fd
Maynard Lee <lxd_ddcome@163.com>
2020-06-12 15:45:04 +0800

    feat: 更新插件配置

--- packages/index.js
+++ packages/index.js
@@ -1,28 +1,23 @@
-// 导入单个组件
-import BlackElTable from './black-el-table/index';
+import BlackElTable from './black-el-table/index'
 
-// 以数组的结构保存组件，便于遍历
 const components = [
 	BlackElTable
-];
+]
 
-// 定义 install 方法
-const install = (Vue) => {
-	if (install.installed) return
-	install.installed = true
-	// 遍历并注册全局组件
+const install = function (Vue) {
+	if (install.installed) return;
+	install.installed = true;
 	components.map(component => {
-		Vue.component(component.name, component)
+		Vue.component(component.name, component);
 	})
-};
+}
 
 if (typeof window !== 'undefined' && window.Vue) {
+	// window.Vue.use(install)
 	install(window.Vue)
 }
 
 export default {
-	// 导出的对象必须具备一个 install 方法
 	install,
-	// 组件列表
-	...components
-};
+	BlackElTable
+}
